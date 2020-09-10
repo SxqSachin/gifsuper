43445473796aec366ada2b6529c2194a75cd2592
SxqSachin <SxqSachin@yahoo.com>
2020-09-10 19:52:23 +0800

    filter

--- src/pages/index/vue/components/panels/filter.vue
+++ src/pages/index/vue/components/panels/filter.vue
@@ -1,19 +1,21 @@
 <template>
   <div class="flex flex-col justify-center items-start mb-0 w-full">
     <div class="img-container flex flex-wrap w-full">
-      <div class="img-wrapper relative p-2 mb-2 flex-0 bg-assets shadow hover:shadow-xl transition-shadow transition-time-func rounded-md cursor-pointer" 
+      <div class="img-wrapper p-2 mb-2 flex-0 bg-assets shadow hover:shadow-xl transition-shadow transition-time-func rounded-md cursor-pointer" 
         v-for="(previewImg, key) in previewImgs"
         :key="key"
-        @click="applyFilter(previewImg.name)"
         >
-        <img class="w-full" :src="previewImg.url" :alt="previewImg.title"/>
-        <div class="filter-info absolute flex justify-center items-center opacity-0 hover:opacity-100 transition-opacity duration-300">
-          <span class="text-gray-400"> {{ previewImg.title }} </span>
-        </div>
+        <div class="w-full relative" @click="applyFilter(previewImg.name)" title="点击已应用该滤镜">
+          <img class="w-full" :src="previewImg.url" :alt="previewImg.title"/>
 
-        <div v-show="filterState[key] && filterState[key].show" class="filter-state absolute flex flex-col justify-center items-center">
-          <p v-show="filterState[key] && filterState[key].title" class="text-gray-400 mb-2"> {{ filterState[key] ? filterState[key].title : '' }} </p>
-          <p v-show="filterState[key] && filterState[key].subtitle" class="text-gray-400"> {{ filterState[key] ? filterState[key].subtitle : '' }} </p>
+          <div class="filter-info absolute flex justify-center items-center opacity-0 transition-opacity duration-300">
+            <span class="text-gray-400"> {{ previewImg.title }} </span>
+          </div>
+
+          <div v-show="filterState[key] && filterState[key].show" class="filter-state absolute flex flex-col justify-center items-center">
+            <p v-show="filterState[key] && filterState[key].title" class="text-gray-400 mb-2"> {{ filterState[key] ? filterState[key].title : '' }} </p>
+            <p v-show="filterState[key] && filterState[key].subtitle" class="text-gray-400"> {{ filterState[key] ? filterState[key].subtitle : '' }} </p>
+          </div>
         </div>
       </div>
     </div>
@@ -44,20 +46,20 @@ export default class extends Vue implements Toasted {
   public filterState: {[type: string]: { show: boolean, title: string, subtitle: string }} = {};
 
   get filters() {
-    const filterList = [
-      { name: 'Noise', type: 'Noise', title: '噪声', options: { noise: 128, }, },
-      { name: 'Grayscale', type: 'Grayscale', title: '灰阶', },
-      { name: 'Invert', type: 'Invert', title: '负片', },
-      { name: 'BlackWhite', type: 'BlackWhite', title: '黑白', },
-      { name: 'Sepia', type: 'Sepia', title: '复古', },
-      { name: 'Pixelate', type: 'Pixelate', title: '像素', options: { blocksize: 8, }, },
-      { name: 'Kodachrome', type: 'Kodachrome', title: '艳丽', },
-      { name: 'Vintage', type: 'Vintage', title: '陈旧', },
-      { name: 'Polaroid', type: 'Polaroid', title: '拍立得画风', },
-      { name: 'Technicolor', type: 'Technicolor', title: '科幻', },
-      { name: 'Sharpen', type: 'Convolute', title: '锐利', options: { matrix: [0, -1, 0, -1, 5, -1, 0, -1, 0], }, },
-      { name: 'Emboss', type: 'Convolute', title: '浮雕', options: { matrix: [1, 1, 1, 1, 0.7, -1, -1, -1, -1], }, },
-    ];
+    const filterList = {
+      'Noise': { name: 'Noise', type: 'Noise', title: '噪声', options: { noise: 128, }, },
+      'Grayscale': { name: 'Grayscale', type: 'Grayscale', title: '灰阶', },
+      'Invert': { name: 'Invert', type: 'Invert', title: '负片', },
+      'BlackWhite': { name: 'BlackWhite', type: 'BlackWhite', title: '黑白', },
+      'Sepia': { name: 'Sepia', type: 'Sepia', title: '复古', },
+      'Pixelate': { name: 'Pixelate', type: 'Pixelate', title: '像素', options: { blocksize: 8, }, },
+      'Kodachrome': { name: 'Kodachrome', type: 'Kodachrome', title: '艳丽', },
+      'Vintage': { name: 'Vintage', type: 'Vintage', title: '陈旧', },
+      'Polaroid': { name: 'Polaroid', type: 'Polaroid', title: '拍立得画风', },
+      'Technicolor': { name: 'Technicolor', type: 'Technicolor', title: '科幻', },
+      'Sharpen': { name: 'Sharpen', type: 'Convolute', title: '锐利', options: { matrix: [0, -1, 0, -1, 5, -1, 0, -1, 0], }, },
+      'Emboss': { name: 'Emboss', type: 'Convolute', title: '浮雕', options: { matrix: [1, 1, 1, 1, 0.7, -1, -1, -1, -1], }, },
+    };
 
     return filterList
   }
@@ -103,10 +105,10 @@ export default class extends Vue implements Toasted {
 
     const img = await parseImgPromise;
 
-    this.filters.forEach(({ type, name, options, title }) => {
+    Object.values(this.filters).forEach(({ type, name, options, title }: any) => {
       const filter = new fabric.Image.filters[type](options);
       img.applyFilters([filter]);
-      this.$set(this.previewImgs, name, { url: img.toDataURL({format: 'png'}), title, type, name })
+      this.$set(this.previewImgs, name, { url: img.toDataURL({format: 'png'}), title, type, name, options })
     });
   }
 
@@ -118,8 +120,12 @@ export default class extends Vue implements Toasted {
 </script>
 
 <style lang="scss" scoped>
-.img-container  .img-wrapper {
+.img-container .img-wrapper {
   width: calc(50% - 0.25rem);
+}
+
+.img-wrapper:hover .filter-info {
+  opacity: 1;
 }
 
 .img-container .img-wrapper:nth-child(even) {
@@ -128,10 +134,10 @@ export default class extends Vue implements Toasted {
 
 .img-wrapper .filter-info,
 .img-wrapper .filter-state {
-  top: 0.5rem;
-  left: 0.5rem;
-  width: calc(100% - 1rem);
-  height: calc(100% - 1rem);
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
   background-color: #333333cc;
 }
 
