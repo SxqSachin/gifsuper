87e4f4f933644e82156eea3878f0abfaed53d365
sxqsachin <sxqsachin@yahoo.com>
2020-12-24 09:10:45 +0800

    up: user feedback

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -12,6 +12,27 @@ import message from '@/js/msg/message.ts';
 import * as Sentry from "@sentry/browser";
 import { Integrations } from "@sentry/tracing";
 
+Sentry.init({
+  dsn: "https://079ceae1879746eea587dae7303ef7ff@o495295.ingest.sentry.io/5567856",
+  autoSessionTracking: true,
+  integrations: [
+    new Integrations.BrowserTracing(),
+  ],
+  beforeSend(event, hint) {
+    // Check if it is an exception, and if so, show the report dialog
+    if (event.exception) {
+      Sentry.showReportDialog({ eventId: event.event_id });
+    }
+
+    return event;
+  },
+
+  tracesSampleRate: 1.0,
+});
+
+// window.onerror = (e) => {
+//   Sentry.captureException(err);
+// }
 
 Vue.prototype.$message = message;
 Vue.config.ignoredElements = [/^ion-/];
@@ -19,18 +40,3 @@ Vue.config.ignoredElements = [/^ion-/];
 new Vue({
   render: h => h(App),
 }).$mount('#app');
-
-Sentry.init({
-  dsn: "https://079ceae1879746eea587dae7303ef7ff@o495295.ingest.sentry.io/5567856",
-  autoSessionTracking: true,
-  integrations: [
-    new Integrations.BrowserTracing(),
-  ],
-
-  tracesSampleRate: 1.0,
-});
-
-
-window.onerror = (e) => {
-  Sentry.captureException(err);
-}
